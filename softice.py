efedc9e5e87870bdf399f46b2b37a446550fd17d
Andrey Pakhomenkov <pakhomenkov.ap@gmail.com>
2022-06-28 14:12:24 +0300

    SoftIce.py переделан на классовые рельсы

--- softice.py
+++ softice.py
@@ -18,7 +18,6 @@ import librarian
 import mafiozo
 import meteorolog
 
-
 INTERVAL: int = 0
 NON_STOP: bool = True
 CONFIG_FILE_NAME: str = "config.json"
@@ -42,11 +41,13 @@ EXIT_COMMANDS: list = ["прощай", "bye"]  # !
 HELP_COMMANDS: list = ["помощь", "help"]  # !
 BABBLER_PERIOD: int = 10  # !
 
+
 # message.delete()
 
 
 class CQuitByDemand(Exception):
     """Исключение выхода."""
+
     def __init__(self):
         self.message = "Выход по требованию..."
         super().__init__(self.message)
@@ -54,11 +55,9 @@ class CQuitByDemand(Exception):
 
 # *** Читаем конфигурацию +++
 with open(CONFIG_FILE_NAME, "r", encoding="utf-8") as config_file:
-
     BOT_CONFIG = json.load(config_file)
 
 if BOT_CONFIG["proxy"]:
-
     apihelper.proxy = {'https': BOT_CONFIG["proxy"]}
 SoftIceBot = telebot.TeleBot(BOT_CONFIG["token"])
 
@@ -71,15 +70,50 @@ class CSoftIceBot:
         super().__init__()
         self.config = {}
         self.last_babbler_phrase_time: datetime = datetime.now()
+        self.load_config()
         self.robot: telebot.TeleBot = telebot.TeleBot(self.config[TOKEN_KEY])
         self.bot_status: int = CONTINUE_RUNNING
         babbler.reload_babbling()
         barman.reload_bar()
         librarian.reload_library()
 
+        # @self.robot.message_handler(commands=["start"])
+        @self.robot.message_handler(content_types=['text'])
+        def process_message(pmessage):
+            """Обработчик сообщений."""
+
+            message_text: str = pmessage.text
+            # word_list: list = func.parse_input(pmessage.text)
+            print("*** ", message_text)
+            command = pmessage.text[1:].lower()
+            chat_id: int = pmessage.chat.id
+            chat_title: str = pmessage.chat.title
+            user_name = pmessage.from_user.username
+            user_title: str = pmessage.from_user.first_name
+            user_id: int = pmessage.from_user.id
+
+            # *** Проверим, легитимный ли этот чата
+            self.check_is_this_chat_enabled(chat_id, chat_title)
+
+            # *** Боту дали команду?
+            if message_text[0:1] in COMMAND_SIGNS:
+
+                if not self.is_reload_config_command_queried(command, chat_id,
+                                                             user_name, user_title):
+
+                    if not self.is_quit_command_queried(command, chat_id,
+                                                        user_name, user_title):
+
+                        if not self.is_help_command_queried(command, chat_id, chat_title):
+                            # *** Передаём введённую команду модулям
+                            self.process_modules(chat_id, chat_title, user_id, user_title, message_text)
+            else:
+
+                self.call_babbler(chat_id, chat_title, message_text)
+
     def call_babbler(self, pchat_id, pchat_title, pmessage_text):
         """Функция болтуна"""
-
+        print("*****************")
         minutes = (datetime.now() - self.last_babbler_phrase_time).total_seconds() / BABBLER_PERIOD
         # *** Заданный период времени с последней фразы прошел?
         if minutes > 1:
@@ -93,7 +127,6 @@ class CSoftIceBot:
 
                     print("Babbler answers.", message)
                     if len(message) > 0:
-
                         SoftIceBot.send_message(pchat_id, message)
                         return True
             self.last_babbler_phrase_time = datetime.now()
@@ -108,7 +141,6 @@ class CSoftIceBot:
             # *** ... точняк
             message = barman.barman(pmessage_text, puser_title)
             if message is not None:
-
                 print("Barman answers.")
                 SoftIceBot.send_message(pchat_id, message)
                 return True
@@ -124,7 +156,6 @@ class CSoftIceBot:
                                           puser_title,
                                           pmessage_text)
             if message is not None:
-
                 print("Librarian answers.")
                 SoftIceBot.send_message(pchat_id, message)
                 return True
@@ -162,7 +193,6 @@ class CSoftIceBot:
             # *** как пить дать.
             message = meteorolog.meteorolog(pmessage_text)
             if message is not None:
-
                 print(" Meteorolog answers.")
                 SoftIceBot.send_message(pchat_id, message)
                 return True
@@ -177,7 +207,6 @@ class CSoftIceBot:
             # *** как пить дать.
             message = theolog.theolog(pmessage_text)
             if message is not None:
-
                 print("Theolog answers.")
                 SoftIceBot.send_message(pchat_id, message)
                 return True
@@ -186,7 +215,6 @@ class CSoftIceBot:
     def check_is_this_chat_enabled(self, pchat_id: int, pchat_title: str):
         """Проверяет, находится ли данный чат в списке разрешенных."""
         if pchat_title not in self.config[ALLOWED_CHATS]:
-
             self.robot.send_message(pchat_id, "Вашего чата нет в списке разрешённых. Чао!")
             self.robot.leave_chat(pchat_id)
             print(f"Караул! Меня похитили и затащили в чат {pchat_title}! Но я удрал.")
@@ -194,30 +222,31 @@ class CSoftIceBot:
     def is_help_command_queried(self, pcommand: str,
                                 pchat_id: int, pchat_title: str):
         """Проверяет, не была ли запрошена подсказка."""
-        SoftIceBot.send_message(pchat_id, HELP_MESSAGE)
         if pcommand in HELP_COMMANDS:
+            barman_message = barman.get_help(self.config, pchat_title)
+            librarian_message = librarian.get_help(self.config, pchat_title)
+            meteorolog_message = meteorolog.help()
+            theolog_message = theolog.get_help(self.config, pchat_title)
+            if (barman_message or
+               librarian_message or
+               meteorolog_message or
+               theolog_message):
+                SoftIceBot.send_message(pchat_id, HELP_MESSAGE)
 
             # *** Помощь от бармена
-            barman_message = barman.get_help(self.config, pchat_title)
             if barman_message:
-
                 SoftIceBot.send_message(pchat_id, barman_message)
 
             # *** Помощь от библиотекаря
-            librarian_message = librarian.get_help(self.config, pchat_title)
             if librarian_message:
-
                 SoftIceBot.send_message(pchat_id, librarian_message)
 
             # *** Помощь от метеоролога
-            meteorolog_message = meteorolog.help()
             if meteorolog_message:
-
                 SoftIceBot.send_message(pchat_id, meteorolog_message)
-            # *** Помощь от теолога
-            theolog_message = theolog.get_help(self.config, pchat_title)
-            if theolog_message:
 
+            # *** Помощь от теолога
+            if theolog_message:
                 SoftIceBot.send_message(pchat_id, theolog_message)
 
     def is_quit_command_queried(self, pcommand: str, pchat_id: int,
@@ -228,7 +257,6 @@ class CSoftIceBot:
         if pcommand in EXIT_COMMANDS:
 
             if puser_name == BOT_CONFIG["master"]:
-
                 self.robot.send_message(pchat_id, "Всем пока!")
                 result = True
                 raise CQuitByDemand()
@@ -256,10 +284,10 @@ class CSoftIceBot:
     def load_config(self):
         """Загружает конфигурацию из JSON."""
         with open(CONFIG_FILE_NAME, "r", encoding="utf-8") as json_file:
-
             self.config = json.load(json_file)
 
     def poll(self):
+        """Функция опроса ботом телеграмма."""
         try:
             while self.bot_status == CONTINUE_RUNNING:
                 # SoftIceBot.infinity_polling()
@@ -273,56 +301,32 @@ class CSoftIceBot:
             # sys.exit()
             # SoftIceBot.stop_polling()
 
-    def process_message(self, pmessage):
-        """Обработчик сообщений."""
-
-        message_text: str = pmessage.text
-        # word_list: list = func.parse_input(pmessage.text)
-        command = pmessage.text[1:].lower()
-        chat_id: int = pmessage.chat.id
-        chat_title: str = pmessage.chat.title
-        user_name = pmessage.from_user.username
-        user_title: str = pmessage.from_user.first_name
-        user_id: int = pmessage.from_user.id
-
-        # *** Проверим, легитимный ли этот чата
-        self.check_is_this_chat_enabled(chat_id, chat_title)
-
-        # *** Боту дали команду?
-        if message_text[0:1] in COMMAND_SIGNS:
-
-            if not self.is_reload_config_command_queried(command, chat_id,
-                                                         user_name, user_title):
-
-                if not self.is_quit_command_queried(command, chat_id,
-                                                    user_name, user_title):
-
-                    if not self.is_help_command_queried(command, chat_id, chat_title):
-
-                        # *** Передаём введённую команду модулям
-                        process_modules(chat_id, chat_title, user_id, user_title, message_text)
-
     def process_modules(self, pchat_id: int, pchat_title: str,
                         puser_id: int, puser_title: str,
                         pmessage_text):
         """Пытается обработать команду различными модулями."""
         # *** Проверим, не запросил ли пользователь что-то у бармена...
+        print("*** bbbbb")
         if not self.call_barman(pchat_id, pchat_title, puser_title, pmessage_text):
 
+            print("*** lll")
             if not self.call_librarian(pchat_id,
                                        pchat_title, puser_title, pmessage_text):
-
                 if not self.call_mafiozo(pchat_id, pchat_title,
                                          puser_id, puser_title, pmessage_text):
+                    print("*** mmm")
                     if not self.call_meteorolog(pchat_id, pchat_title, pmessage_text):
 
+                        print("*** ttt")
                         if not self.call_theolog(pchat_id, pchat_title, pmessage_text):
 
+                            print("*** bbb")
                             if not self.call_babbler(pchat_id, pchat_title, pmessage_text):
-
                                 print(" .. fail.")
 
+
 NEW_ROBOT = CSoftIceBot()
+
 
 def send_help(pconfig: dict, pmessage):  # +++
     """По запросу пользователя отправляет подсказки пло всем модулям."""
@@ -332,16 +336,12 @@ def send_help(pconfig: dict, pmessage):  # +++
     librarian_message = librarian.get_help(pconfig, pmessage.chat.title)
 
     if barman_message or theolog_message or librarian_message:
-
         SoftIceBot.send_message(pmessage.chat.id, HELP_MESSAGE)
     if barman_message:
-
         SoftIceBot.send_message(pmessage.chat.id, barman_message)
     if theolog_message:
-
         SoftIceBot.send_message(pmessage.chat.id, theolog_message)
     if librarian_message:
-
         SoftIceBot.send_message(pmessage.chat.id, librarian_message)
 
 
@@ -360,7 +360,6 @@ def babbler_process(pconfig: dict, pchat_id: int, pchat_title: str,
 
             print(" .. ok.", message)
             if len(message) > 0:
-
                 SoftIceBot.send_message(pchat_id, message)
                 return True
     return False
@@ -376,7 +375,6 @@ def barman_process(pconfig: dict, pchat_id: int, pchat_title: str,
         # message = barman.barman(pmessage.text, pmessage.from_user.user_name)
         message = barman.barman(pmessage_text, puser_title)
         if message is not None:
-
             print(" .. ok.")
             SoftIceBot.send_message(pchat_id, message)
             return True
@@ -392,7 +390,6 @@ def theolog_process(pconfig: dict, pchat_id: int, pchat_title: str,
         # *** как пить дать.
         message = theolog.theolog(pmessage_text)
         if message is not None:
-
             print(" .. ok.")
             SoftIceBot.send_message(pchat_id, message)
             return True
@@ -407,7 +404,6 @@ def meteorolog_process(pconfig: dict, pchat_id: int, pchat_title: str,
         # *** как пить дать.
         message = meteorolog.meteorolog(pmessage_text)
         if message is not None:
-
             print(" .. ok.")
             SoftIceBot.send_message(pchat_id, message)
             return True
@@ -424,7 +420,6 @@ def librarian_process(pconfig: dict, pchat_id: int, pchat_title: str,
                                       puser_title,
                                       pmessage_text)
         if message is not None:
-
             print(" .. ok.")
             SoftIceBot.send_message(pchat_id, message)
             return True
@@ -476,7 +471,6 @@ def process_modules(pchat_id: int, pchat_title: str,
 
                     if not mafiozo_process(BOT_CONFIG, pchat_id, pchat_title,
                                            puser_id, puser_title, pmessage_text):
-
                         print(" .. fail.")
 
                 # SoftIceBot.send_message(pmessage.chat.id,
@@ -517,7 +511,6 @@ def bot_command(pmessage):
     elif command in ["прощай", "bye"]:
 
         if user_name == BOT_CONFIG["master"]:
-
             SoftIceBot.send_message(chat_id, "Всем пока!")
             raise CQuitByDemand()
         SoftIceBot.send_message(chat_id, f"Извини, {user_title}, у меня другой хозяин!")
@@ -535,10 +528,15 @@ def callback_inline(call):
                     call.from_user.id, call.from_user.username, call.data)
 
 
-@SoftIceBot.message_handler(func=lambda message: True, content_types=['text'])
+# @SoftIceBot.message_handler(func=lambda message: True, content_types=['text'])
+# @NEW_ROBOT.message_handler(func=lambda message: True, content_types=['text'])
 def get_text_messages(pmessage):
     """Процедура обработки ввода команд пользователем."""
+    # print("### ", pmessage.text)
+    # NEW_ROBOT.process_message(pmessage)
 
+
+"""
     global BOT_CONFIG
     word_list: list = func.parse_input(pmessage.text)
     message_text: str = pmessage.text
@@ -548,12 +546,12 @@ def get_text_messages(pmessage):
     chat_title: str = pmessage.chat.title
     # print(chat_id, chat_title)
     # *** Проверка разрешенных каналов
-    print(f"{user_title} в {chat_title} сказал: ", message_text)
+    # print(f"{user_title} в {chat_title} сказал: ", message_text)
     if chat_title not in BOT_CONFIG[ALLOWED_CHATS]:
 
-        SoftIceBot.send_message(chat_id, "Вашего чата нет в списке разрешённых. Чао!")
+        # SoftIceBot.send_message(chat_id, "Вашего чата нет в списке разрешённых. Чао!")
         SoftIceBot.leave_chat(chat_id)
-        print(f"Караул! Меня похитили и затащили в чат {chat_title}! Но я удрал.")
+        # print(f"Караул! Меня похитили и затащили в чат {chat_title}! Но я удрал.")
 
     # *** В начале строки есть знак команды?
     if message_text[0:1] in COMMAND_SIGNS:
@@ -584,11 +582,11 @@ def get_text_messages(pmessage):
 
                 babbler_process(BOT_CONFIG, chat_id, chat_title, message_text)  # , user_title
                 LAST_BABBLER_PHRASE_TIME = datetime.now()
-
+"""
 
 if __name__ == "__main__":
-
     # ------------------------
+    """
     babbler.reload_babbling()
     # babbler.reload_babling_ext()
     barman.reload_bar()
@@ -606,4 +604,5 @@ if __name__ == "__main__":
         sys.exit()
         # SoftIceBot.stop_polling()
     # -----------------------
+    """
     NEW_ROBOT.poll()
