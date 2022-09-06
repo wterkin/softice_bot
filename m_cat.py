419fe020a1b097b45f083c42fd1cc5cd570f306c
wterkin <appmailbox@mail.ru>
2022-09-06 19:38:19 +0300

    Кошки-1

--- m_cat.py
+++ m_cat.py
@@ -4,7 +4,7 @@
 from sqlalchemy import Column, Integer, String
 
 import m_ancestor
-
+import m_users
 
 class CFeed(m_ancestor.CAncestor):
     """Класс моделей для игры в кошек."""
@@ -25,19 +25,43 @@ class CFeed(m_ancestor.CAncestor):
                    Chat ID:{self.fname}
                    Chat Name:{self.fprice}"""
 
+class CInterior(m_ancestor.CAncestor):
+    """Класс моделей для игры в кошек."""
+
+    __tablename__ = 'tbl_interior'
+    fcolor = Column(String, nullable=False)
+    fwooliness = Column(Integer, nullable=False)
+    fprice = Column(Integer, nullable=False)
+
+    def __init__(self, pname: str, pprice: int):
+        """Конструктор"""
+        super().__init__()
+        self.fname = pname
+        self.fprice = pprice
+
+    def __repr__(self):
+        ancestor_repr = super().__repr__()
+        return f"""{ancestor_repr},
+                   Chat ID:{self.fname}
+                   Chat Name:{self.fprice}"""
+
+
 
 
 class CCat(m_ancestor.CAncestor):
     """Класс модели кошки."""
 
     __tablename__ = 'tbl_cats'
+    fuserid = Column(Integer, ForeignKey(m_users.CUser.id))
     fname = Column(String, nullable=False, default="Мурзик")
+    fcolor = Column(String, nullable=False)
+    fwooliness = Column(String, nullable=False)
+    fbreed = Column(String, nullable=False)
     fhealth = Column(Integer, nullable=False, default=25)
     fstrength = Column(Integer, nullable=False, default=1)
     fsatiety = Column(Integer, nullable=False, default=25)
     fmood = Column(Integer, nullable=False, default=25)
     fdiscipline = Column(Integer, nullable=False, default=25)
-    fexterior = Column(Integer, nullable=False)  # ссылка на справочник внешностей
 
     def __init__(self, pname: str, pprice: int):
         """Конструктор"""
@@ -47,6 +71,9 @@ class CCat(m_ancestor.CAncestor):
         ancestor_repr = super().__repr__()
         return f"""{ancestor_repr},
                    Name:{self.fname},
+                   Color:{self.fcolor},
+                   Wooliness:{self.fwooliness},
+                   Breed:{self.fbreed},
                    Health:{self.fhealth},
                    Strength:{self.fstrength},
                    Satiety:{self.fsatiety},
