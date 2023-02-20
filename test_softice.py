d89d3731f29d552752170c38617681d9a98fc104
Andrey Pakhomenkov <pakhomenkov.ap@gmail.com>
2023-02-20 17:47:01 +0300

    мучаю тесты

--- test_softice.py
+++ test_softice.py
@@ -18,21 +18,28 @@ class CTestSoftIceBot(TestCase):
         self.assertEqual(self.bot.is_this_chat_enabled('supermegachat'), False)
 
     def test_process_command(self):
+        print("test_process_command:user ok, config")
         self.assertEqual(self.bot.process_command("config", -583831606, "superchat",
                                                   {"name": "username", "title": "usertitle"}), True)
+        print("test_process_command:user wrong, config")
         self.assertEqual(self.bot.process_command("config", -583831606, "superchat",
                                                   {"name": "MegaUser", "title": "Юзер"}), False)
+        print("test_process_command:user ok, help")
         self.assertEqual(self.bot.process_command("help", -583831606, "superchat",
                                                   {"name": "username", "title": "usertitle"}), True)
+        print("test_process_command:user wrong, unknown command")
         self.assertNotEqual(self.bot.process_command("вон!", -583831606, "superchat",
                                                      {"name": "username", "title": "usertitle"}), True)
 
     def test_process_modules(self):
         self.bot.message_text = "!Экспекто патронум"
+        print("test_process_modules:??")
         self.assertEqual(self.bot.process_modules(-583831606, "Ботовка", "Pet_Rovich", "Петрович"),
                          ("", False))
 
     def test_reload_config(self):
         # pchat_id: int, puser_name: str, puser_title: str
+        print("test_reload_config:user ok")
         self.assertEqual(self.bot.reload_config(-583831606, "username", "usertitle"), True)
+        print("test_reload_config:user wrong")
         self.assertEqual(self.bot.reload_config(-583831606, "user", "user"), False)
